afcd20eebe246b5ca0c0c6a97c631958657af600
damexix <damexix7@gmail.com>
2024-01-01 22:07:36 +0000

    inital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,63 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import '@cds/core/icon/register.js';
+import { ClarityIcons, usersIcon, bundleIcon, shoppingCartIcon,plusIcon } from '@cds/core/icon';
+import { SignalrService } from './Services/Signalr.Service';
+import { Observable, Subscription, fromEvent, map, merge, of } from 'rxjs';
+import { paymentService } from './Services/PaymentService';
+import { AppModule } from './app.module';
+
+ClarityIcons.addIcons(usersIcon, bundleIcon, shoppingCartIcon, plusIcon);
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit, OnDestroy {
+  networkStatus: boolean = false;
+  networkStatus$: Subscription = Subscription.EMPTY;
+
+  isAuthenticated?: Observable<boolean>
+  allFeedSubscription: any;
+  order: any;
+  show: any = false;
+  constructor(private signalrService: SignalrService, private paymentSrv: paymentService) {}
+
+
+  ngOnDestroy(): void {
+   this.networkStatus$.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.paymentSrv.createTerminal();
+    this.checkNetworkStatus();
+
+    this.signalrService.init();
+
+    // 2 - register for ALL relay
+    this.signalrService.listenToAllFeeds();
+
+    // 3 - subscribe to messages received
+    this.allFeedSubscription = this.signalrService.AllFeedObservable
+      .subscribe((res: any) => {
+        console.log(res);
+      });
+
+  }
+
+  checkNetworkStatus() {
+    this.networkStatus = navigator.onLine;
+    this.networkStatus$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+  fromEvent(window, 'offline')
+)
+  .pipe(map(() => navigator.onLine))
+  .subscribe(status => {
+        console.log('status', status);
+    this.networkStatus = status;
+  });
+}
+
+
+  title = 'Foodloyale';
 }
