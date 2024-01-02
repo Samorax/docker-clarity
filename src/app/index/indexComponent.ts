80e9671be9e31fab4ff7ae7b7343c9c690185574
damexix <damexix7@gmail.com>
2024-01-02 19:51:39 +0000

    homepage designed moderately1

---
+++ src/app/index/indexComponent.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-index',
+    templateUrl:'./indexComponent.html',
+    styleUrl:'./indexComponent.css'
+})
+
+export class indexComponent{
+    
+}
