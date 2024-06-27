a38d4ab2610b51c5634fb1b13e6359c0b8390a18
Samuel Azeez <sam-azeez@foodloyale.com>
2024-06-27 23:22:49 +0100

    partner and slaes from 98 percent done

--- src/app/index/contactSalesComponent.ts
+++ src/app/index/contactSalesComponent.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
 import { ContactService } from "../Services/ContactService";
 import { ClrLoadingState } from "@clr/angular";
@@ -6,12 +6,15 @@ import { ClrLoadingState } from "@clr/angular";
 
 @Component({
     selector:'app-contactSales',
-    templateUrl:'./contactSalesComponent.html'
+    templateUrl:'./contactSalesComponent.html',
+    changeDetection:ChangeDetectionStrategy.OnPush
 })
 
 export class contactSalesComponent{
     contactSalesBtn: ClrLoadingState = ClrLoadingState.DEFAULT;
-    constructor(private _formBuilder:FormBuilder,private _contactSalesSVR: ContactService){}
+    feedbackMessageSuccess!: string;
+    feedbackMessageError!: string;
+    constructor(private _formBuilder:FormBuilder,private _contactSalesSVR: ContactService, private cd:ChangeDetectorRef){}
     feedbackMessage!:string;
     salesForm = this._formBuilder.group({
         firstName:['',Validators.required],
@@ -26,10 +29,10 @@ export class contactSalesComponent{
     onSubmit(){
         this.contactSalesBtn = ClrLoadingState.LOADING;
         let m = this.salesForm.value;
-        this._contactSalesSVR.sendSalesContactForm(m).subscribe((r:any)=>{
-        console.log(r)
-            this.contactSalesBtn = ClrLoadingState.SUCCESS;
-            this.feedbackMessage = r;
-        },(er:Error)=> this.feedbackMessage = er.message);
+        this._contactSalesSVR.sendSalesContactForm(m).subscribe({
+            next:()=>{this.feedbackMessageSuccess ="Success!!! We will get back to you within 24 hours";this.salesForm.disable() ;this.cd.detectChanges()},
+            error:(er:Error)=> {this.feedbackMessageError = er.message; this.cd.detectChanges()},
+            complete:()=>{this.contactSalesBtn = ClrLoadingState.SUCCESS; this.cd.detectChanges()}
+        });
     }
 }
