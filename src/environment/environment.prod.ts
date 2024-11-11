9eb0622377f3c53471d799cc6d3fd9fd78551450
Samuel Azeez <sam-azeez@foodloyale.com>
2024-11-11 13:18:13 +0000

    latest

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    apiBaseUrl:'https://foodloyaleopenapi.azurewebsites.net/',
+    apiBaseUrl:'https://api.foodloyale.com/',
     stripeTestKey:'pk_live_51OZtsPLMinwGqDJeZudyxggWymXrgEUv4rtBEWTBZw9qXWBsk3W7vjWx7gNtVo93CqkHfluo0bScchxA5NaGJ0JM00J3nbSyBS'
 }
